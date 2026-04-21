08a579d4d13ca88b01de3cea12f29652c8706c3e
miguel2107-bip <miguel1429da@gmail.com>
2026-04-20 22:12:23 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function Entrar() {
+    window.location.href = "home.html";
+}
+
+function Registrar() {
+    window.location.href = "registrar.html"
+}
+
+function Logar () {
+    window.location.href = "login.html"
+}
