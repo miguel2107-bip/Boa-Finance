3f49f9b29159290f9851eca26bb8d1083ebd8315
Iagocs222 <iagocarlossouza@gmail.com>
2026-04-21 12:06:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function Registrar() {
     window.location.href = "registrar.html"
 }
 
-function Logar () {
+function Logar() {
+    window.location.href = "login.html"
+}
+function Register() {
+    window.location.href = "home.html";
+}
+function Esqueci() {
+    window.location.href = "registrar.html"
+}
+function Inicio() {
     window.location.href = "login.html"
 }
